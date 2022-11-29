d812a125b00da454988e3246587784d508eee02f
Tusar <tusarahmed141@gmail.com>
2022-11-29 12:18:10 +0600

    Update custom styles

--- src/Styled_Components/Custom.styles.js
+++ src/Styled_Components/Custom.styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Title from "../Title";
+import Title from "../Components/Title";
 
 const StyledTitle = styled(Title)`
   color: ${(props) => props.color};
